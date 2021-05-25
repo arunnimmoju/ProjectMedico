922c14f2fc7f5a1a6fa6e90a709e126231eaebe4
arun <arun.nimmoju1999@gmail.com>
2021-05-25 06:41:57 +0530

    user details storing and retrieving using CLoud Storage Profile image storing and view using Firebase Storage

--- app/src/main/java/com/example/demoapp/HealthReportActivity.java
+++ app/src/main/java/com/example/demoapp/HealthReportActivity.java
@@ -1,28 +1,100 @@
 package com.example.demoapp;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.squareup.picasso.Picasso;
 
 public class HealthReportActivity extends AppCompatActivity {
 
+    TextView userName;
+    TextView userGender;
+    TextView userAge;
+    TextView userHeight;
+    TextView userWeight;
+    TextView userNumber;
+    TextView userOccupation;
+    TextView userBp;
     Button updateProfile;
+    ImageView profileImage;
+
+    FirebaseFirestore fStore;
+    String userId;
+    FirebaseAuth fAuth;
+    StorageReference storageReference;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_health_report);
 
-        updateProfile= findViewById(R.id.editProfile);
+        updateProfile = findViewById(R.id.editProfile);
         updateProfile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent= new Intent(HealthReportActivity.this,updateProfileActivity.class);
+                Intent intent = new Intent(HealthReportActivity.this, updateProfileActivity.class);
                 startActivity(intent);
             }
         });
+
+        userName = findViewById(R.id.viewName);
+        userGender = findViewById(R.id.viewGender);
+        userAge = findViewById(R.id.viewAge);
+        userHeight = findViewById(R.id.viewHeight);
+        userWeight = findViewById(R.id.viewWeight);
+        userBp = findViewById(R.id.viewBp);
+        userNumber = findViewById(R.id.viewPhone);
+        userOccupation = findViewById(R.id.viewOccupation);
+
+
+        fAuth = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
+        storageReference = FirebaseStorage.getInstance().getReference();
+
+        userId = fAuth.getCurrentUser().getUid();
+
+        DocumentReference documentReference = fStore.collection("User").document(userId);
+        documentReference.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException e) {
+                userName.setText(documentSnapshot.getString("Name"));
+                userGender.setText(documentSnapshot.getString("Gender"));
+                userAge.setText(documentSnapshot.getString("Age"));
+                userHeight.setText(documentSnapshot.getString("Height"));
+                userWeight.setText(documentSnapshot.getString("Weight"));
+                userBp.setText(documentSnapshot.getString("BP"));
+                userNumber.setText(documentSnapshot.getString("Contact"));
+                userOccupation.setText(documentSnapshot.getString("Occupation"));
+            }
+        });
+        profileImage = findViewById(R.id.userImage);
+        StorageReference fileRef = storageReference.child("users/"+fAuth.getCurrentUser().getUid()+"/profile.jpg");
+        fileRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+            @Override
+            public void onSuccess(Uri uri) {
+                Picasso.get().load(uri).into(profileImage);
+            }
+        });
+
+
+
     }
 }
--- app/src/main/java/com/example/demoapp/updateProfileActivity.java
+++ app/src/main/java/com/example/demoapp/updateProfileActivity.java
@@ -1,31 +1,210 @@
 package com.example.demoapp;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.example.demoapp.model.User;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
 public class updateProfileActivity extends AppCompatActivity {
 
+    public static final String TAG = "TAG";
+    TextInputEditText uName;
+    AutoCompleteTextView uGender;
+    TextInputEditText uAge;
+    TextInputEditText uHeight;
+    TextInputEditText uWeight;
+    TextInputEditText uOccupation;
+    AutoCompleteTextView uBloodPressure;
+    TextInputEditText uNumber;
+    Button btnUpdateDetails;
+
+    FirebaseFirestore fstore;
+    FirebaseAuth firebaseAuth;
+    StorageReference storageReference;
+    String userId;
+
+    ImageView updateProfileImage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_update_profile);
 
 
-        String[] gender = new String[] {"Male","Female"};
+        uName = findViewById(R.id.updateName);
+        uAge = findViewById(R.id.updateAge);
+        uHeight = findViewById(R.id.updateHeight);
+        uWeight = findViewById(R.id.updateWeight);
+        uOccupation = findViewById(R.id.updateOccupation);
+        uNumber = findViewById(R.id.updatePhone);
+        btnUpdateDetails = findViewById(R.id.updateDetails);
+        uGender = findViewById(R.id.updateGender);
+        uBloodPressure = findViewById(R.id.updateBp);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        fstore= FirebaseFirestore.getInstance();
+        storageReference = FirebaseStorage.getInstance().getReference();
+
+        String[] gender = new String[]{"Male", "Female"};
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.dropdown_menu_popup_item, gender);
-        AutoCompleteTextView editTextFilledExposedDropdown = findViewById(R.id.updateGender);
-        editTextFilledExposedDropdown.setAdapter(adapter);
+        uGender.setAdapter(adapter);
 
-        String[] bloodPressure = new String[] {"Low","High","Normal"};
+        String[] bloodPressure = new String[]{"Low", "High", "Normal"};
         ArrayAdapter<String> adapter1 = new ArrayAdapter<>(this, R.layout.dropdown_menu_popup_item, bloodPressure);
-        AutoCompleteTextView editTextFilledExposedDropdown1 = findViewById(R.id.updateBp);
-        editTextFilledExposedDropdown1.setAdapter(adapter1);
+        uBloodPressure.setAdapter(adapter1);
+
+        btnUpdateDetails.setOnClickListener(v -> {
+            String fName = uName.getText().toString();
+            String fAge = uAge.getText().toString();
+            String fHeight = uHeight.getText().toString();
+            String fWeight = uWeight.getText().toString();
+            String fOccupation = uOccupation.getText().toString();
+            String fNumber = uNumber.getText().toString();
+            String fGender = uGender.getText().toString();
+            String fBp = uBloodPressure.getText().toString();
+
+            if(TextUtils.isEmpty(fName)){
+                uName.setError("User Name is Required");
+                return;
+            }
+            if(TextUtils.isEmpty(fGender)){
+                uGender.setError("Gender is Required");
+                return;
+            }
+            if(TextUtils.isEmpty(fAge)){
+                uAge.setError("Age is Required");
+                return;
+            }
+            if(TextUtils.isEmpty(fHeight)){
+                uHeight.setError("Height is Required");
+                return;
+            }
+
+            if(TextUtils.isEmpty(fWeight)){
+                uWeight.setError("Weight is Required");
+                return;
+            }
+            if(TextUtils.isEmpty(fBp)){
+                uBloodPressure.setError("BP is Required");
+                return;
+            }
+            if(TextUtils.isEmpty(fNumber)){
+                uNumber.setError("Number is Required");
+                return;
+            }
+            if(TextUtils.isEmpty(fOccupation)){
+                uOccupation.setError("Occupation is Required");
+                return;
+            }
+
+            userId = firebaseAuth.getCurrentUser().getUid();
+            DocumentReference documentReference = fstore.collection("User").document(userId);
+
+            Map<String,Object> user= new HashMap<>();
+            user.put("Name",fName);
+            user.put("Gender",fGender);
+            user.put("Age",fAge);
+            user.put("Height",fHeight);
+            user.put("Weight",fWeight);
+            user.put("BP",fBp);
+            user.put("Contact",fNumber);
+            user.put("Occupation",fOccupation);
+
+            documentReference.set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+                @Override
+                public void onSuccess(Void unused) {
+                    Log.d(TAG, "onSuccess: User Data is stored Successfully "+ userId );
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Log.e(TAG, "onFailure: "+ e.toString());
+                }
+            });
+            startActivity(new Intent(getApplicationContext(),HealthReportActivity.class));
+        });
+
+
+        updateProfileImage = findViewById(R.id.updateImage);
+
+        updateProfileImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //open gallery;
+                Intent openGallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI );
+                startActivityForResult(openGallery,1000);
+            }
+        });
+
+        StorageReference profileRef = storageReference.child("users/"+firebaseAuth.getCurrentUser().getUid()+"/profile.jpg");
+        profileRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+            @Override
+            public void onSuccess(Uri uri) {
+                Picasso.get().load(uri).into(updateProfileImage);
+            }
+        });
 
 
 
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 1000){
+            if(resultCode == Activity.RESULT_OK){
+                Uri imageUri = data.getData();
+                updateProfileImage.setImageURI(imageUri);
+                uploadImageToDatabase(imageUri);
+
+            }
+        }
+    }
+
+    private void uploadImageToDatabase(Uri imageUri) {
+        //uploading image to firebase;
+        StorageReference fileRef = storageReference.child("users/"+firebaseAuth.getCurrentUser().getUid()+"/profile.jpg");
+        fileRef.putFile(imageUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                Toast.makeText(updateProfileActivity.this, "Image Uploaded", Toast.LENGTH_SHORT).show();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(updateProfileActivity.this, "Upload Failed", Toast.LENGTH_SHORT).show();
+            }
+        });
 
     }
 }
